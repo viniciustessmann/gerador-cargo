9d0ec77aaa5d6f9d6e9b27a89b2df9695ac89bff
Vinícius Tessmann <vinicius@melhorenvio.com.br>
2019-07-24 20:15:09 -0300

    Criar função de gerar cargo

--- src/Gerador/GeradorCargo.php
+++ src/Gerador/GeradorCargo.php
@@ -4,8 +4,42 @@ namespace Gerador;
 
 class GeradorCargo
 {
+
     public static function gerar()
     {
-        return 'Desenvolvedor Backend Master';
+        return $this->getCargo() . $this->getTipo() . $this->getNivel();   
+    }
+
+    private function getCargo()
+    {
+        $cargos = [
+            'Programador ',
+            'Desenvolvedor ',
+            'Analista ',
+            'Tester '        
+        ];
+
+        return array_rand($cargos, 1);
+    }
+
+    private function getTipo()
+    {
+        $tipos = [
+            'Backend ',
+            'Frontend ',
+        ];
+
+        return array_rand($tipos, 1);
+    }
+
+    private function getNivel()
+    {
+        $nivel = [
+            'Master',
+            'Sênior',
+            'Pleno',
+            'Junior',
+            'jr'
+        ];
     }
 }
